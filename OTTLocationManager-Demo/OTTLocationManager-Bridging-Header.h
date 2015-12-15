47bca4caa3defbabbc428b49e35dba2760514b48
twairball <twairball@yahoo.com>
2015-12-15 12:06:41 +0800

    repackaged for OTTLocationManager namespacing

---
+++ OTTLocationManager-Demo/OTTLocationManager-Bridging-Header.h
@@ -0,0 +1,11 @@
+//
+//  OTTLocationManager-Bridging-Header.h
+//  OTTLocationManager
+//
+//  Created by impressly on 12/3/15.
+//  Copyright © 2015 OTT. All rights reserved.
+//
+
+#import <CoreLocation/CoreLocation.h>
+#import <MapKit/MapKit.h>
+#import <AddressBook/AddressBook.h>
