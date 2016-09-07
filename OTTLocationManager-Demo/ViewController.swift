2f14744d573139bc760c8d30498f01db45373179
twairball <twairball@yahoo.com>
2016-09-08 00:39:09 +0800

    location and geocode methods

--- OTTLocationManager-Demo/ViewController.swift
+++ OTTLocationManager-Demo/ViewController.swift
@@ -7,60 +7,100 @@
 //
 
 import UIKit
+import AddressBook
 
 class ViewController: UIViewController {
     var locationManager = OTTLocationManager()
 
+    // location geocoding
     @IBOutlet weak var lat: UILabel!
     @IBOutlet weak var lon: UILabel!
     @IBOutlet weak var city: UILabel!
     @IBOutlet weak var district: UILabel!
+    @IBOutlet weak var country: UILabel!
+    
+    @IBAction func didTapRefresh(sender: AnyObject) {
+        refresh()
+    }
+    
+    // reverse geocoding address
+    
+    @IBOutlet weak var addressTextField: UITextField!
+    @IBAction func didTapAddressGeocode(sender: AnyObject) {
+        lookupAddress()
+    }
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.locationManager.delegate = self
         refresh()
-        
-        
     }
 
     func refresh() {
-        
-        //开启定位
-        self.locationManager.setup()
-        self.locationManager.getLatLon()
+        locationManager.start()
     }
     
-    func getNextPage() {
-        
-        if self.locationManager.mode == OTTLocationManagerMode.latLon {
-            let lat = Float(self.locationManager.currentLocation!.coordinate.latitude)
-            
-            let lon = Float(self.locationManager.currentLocation!.coordinate.longitude)
-           
-        } else if self.locationManager.mode == OTTLocationManagerMode.cityDistrict {
-            
-            let city = self.locationManager.city
-            let district = self.locationManager.district
-            
-//            self.city.text = city
-//            self.district.text = district
-        }
-
+    func lookupAddress() {
+        guard let address = addressTextField.text else { return }
+//        locationManager.geocode(address)
+        locationManager.geocodeDitu(address)
     }
 
 }
 
 
 extension ViewController : OTTLocationManagerDelegate {
-    func didUpdateLatLon(locationManager: OTTLocationManager, lat: Float, lon: Float) {
-        
+    
+    func didUpdateLatLon(locationManager: OTTLocationManager, lat: Double, lon: Double) {
         self.lat.text = lat.description
         self.lon.text = lon.description
     }
     
-    func didSetCityDistrict(locationManager: OTTLocationManager, city: String, district: String) {
+    func didUpdateCityDistrict(locationManager: OTTLocationManager, district: String?, city: String?, country: String?) {
         self.city.text = city
         self.district.text = district
+        self.country.text = country
+    }
+    
+    func didGeocodeAddress(locationManager: OTTLocationManager, placemarks: [CLPlacemark]) {
+        if placemarks.count == 0 {
+            alertNoResults()
+        } else {
+            for pm in placemarks {
+                print("placemark: \(pm.country), \(pm.locality), \(pm.subLocality)")
+                let anno = MKPlacemark(placemark: pm)
+                self.mapView.addAnnotation(anno)
+            }
+        }
+    }
+    
+    func didGeocodeDitu(locationManager: OTTLocationManager, results: [Dictionary<String, AnyObject>]) {
+        if results.count == 0 {
+            alertNoResults()
+        } else {
+            for r in results {
+                let address = r["address"] as! String
+                let lat = r["lat"] as! Double
+                let lon = r["lon"] as! Double
+                let coord = CLLocationCoordinate2DMake(lat, lon)
+                let addressDict: Dictionary<String, AnyObject> = [kABPersonAddressStreetKey as String: address]
+                
+                let anno = MKPlacemark(coordinate: coord, addressDictionary: addressDict)
+                self.mapView.addAnnotation(anno)
+            }
+        }
+    }
+    
+    func alertNoResults() {
+        // alert no results
+        let alertVC = UIAlertController(title: "Geocode", message: "No results found", preferredStyle: UIAlertControllerStyle.Alert)
+        let action = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil)
+        alertVC.addAction(action)
+        
+        presentViewController(alertVC, animated: true, completion: nil)
     }
 }
 
--- OTTLocationManager/OTTLocationManager.swift
+++ OTTLocationManager/OTTLocationManager.swift
@@ -10,14 +10,12 @@ import Foundation
 import Foundation
 import CoreLocation
 
-enum OTTLocationManagerMode {
-    case latLon
-    case cityDistrict
-}
-
 protocol OTTLocationManagerDelegate: NSObjectProtocol {
-    func didUpdateLatLon(locationManager: OTTLocationManager, lat: Float, lon: Float)
-    func didSetCityDistrict(locationManager: OTTLocationManager, city: String, district: String)
+    func didUpdateLatLon(locationManager: OTTLocationManager, lat: Double, lon: Double)
+    func didUpdateCityDistrict(locationManager: OTTLocationManager, district: String?, city: String?, country: String?)
+    
+    func didGeocodeAddress(locationManager: OTTLocationManager, placemarks: [CLPlacemark])
+    func didGeocodeDitu(locationManager: OTTLocationManager, results: [Dictionary<String, AnyObject>])
 }
 
 class OTTLocationManager: NSObject {
@@ -29,35 +27,23 @@ class OTTLocationManager: NSObject {
     //LocationManagerDelegate
     weak var delegate: OTTLocationManagerDelegate?
     
-    // user settings
-    var mode: OTTLocationManagerMode
-    
-    // city/district mode
-    var city: String
-    var district: String
+    // geocoded city/country/district
+    var district: String?
+    var city: String?
+    var country: String?
     
     // MARK: Lifecycle
     override init() {
         self.currentLocation = CLLocation()
-        self.mode = OTTLocationManagerMode.latLon
-        city = "上海"
-        district = "徐汇区"
-        
         super.init()
     }
     
     deinit {
         manager.stopUpdatingLocation()
     }
-    
-    // MARK: Setup
-    func setup() {
-        // TODO: user default read -- Mode, lat/lon or city/district
-    }
-    
-    //MARK:IOS8 以后才能使用定位功能
+
+    // IOS8 以后才能使用定位功能
     private func ios8() -> Bool {
-        
         return UIDevice.currentDevice().systemVersion >= "8.0"
     }
     
@@ -65,15 +51,15 @@ class OTTLocationManager: NSObject {
     func allowRequest() {
         if (ios8()) {
             
-            //始终允许访问位置信息
-            self.manager.requestAlwaysAuthorization()
-            
-            //使用应用程序期间允许访问位置数据
+            // 使用应用程序期间允许访问位置数据
             self.manager.requestWhenInUseAuthorization()
+            
+        } else {
+            print("[OTTLocationMangager allowRequest] iOS7 or below not yet implemented! Failed to get user authorization for location")
         }
     }
     
-    func getLatLon() {
+    func start() {
         self.allowRequest()
         if (CLLocationManager.locationServicesEnabled()) {
             self.manager.desiredAccuracy = kCLLocationAccuracyBest
@@ -85,33 +71,98 @@ class OTTLocationManager: NSObject {
         }
     }
     
-    // TODO: Save/Load settings to user default
+    func stop() {
+        manager.stopUpdatingLocation()
+    }
     
+    // MARK: - Geocoding
+    func geocode(address: String) {
+        CLGeocoder().geocodeAddressString(address, completionHandler: {(placemarks, error) in
+            if (error != nil) {
+                print("[OTTLocationManager didUpdateLocations] reverse geocoder failed")
+                return
+            }
+            
+            if placemarks!.count > 0 {
+                self.delegate?.didGeocodeAddress(self, placemarks: placemarks!)
+            }
+        })
+    }
+    
+    func geocodeDitu(address: String) {
+        let escAddress = address.stringByReplacingOccurrencesOfString(" ", withString: "+").stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)!
+        let url = NSURL(string: "http://ditu.google.cn/maps/api/geocode/json?address=\(escAddress)&sensor=false")
+        
+        var jsonDict: NSDictionary = NSDictionary()
+        
+        do {
+            let resp = NSData(contentsOfURL: url!)
+            jsonDict = try NSJSONSerialization.JSONObjectWithData(resp!, options: NSJSONReadingOptions.MutableContainers) as! NSDictionary
+            jsonDict["result"]
+        } catch {
+            print("[geocodeDitu] error: \(error)")
+        }
+        
+        var results: [Dictionary<String, AnyObject>] = []
+        
+        for json in (jsonDict["results"] as! NSArray) {
+            let r = json as! Dictionary<String, AnyObject>
+            
+            let formattedAddress = r["formatted_address"] as! String
+            print("[geocodeDitu] formatted: \(formattedAddress)")
+            
+            let geometry = r["geometry"] as! Dictionary<String, AnyObject>
+            let loc = geometry["location"] as! Dictionary<String, AnyObject>
+            let lat = loc["lat"] as! Double
+            let lon = loc["lng"] as! Double
+            print("          lat/lng: \(lat), \(lon)")
+            
+            // append result
+            
+            let result: Dictionary<String, AnyObject> = ["address": formattedAddress, "lat": lat, "lon": lon]
+            results.append(result)
+        }
+        
+        delegate?.didGeocodeDitu(self, results: results)
+    }
 }
 
 extension OTTLocationManager: CLLocationManagerDelegate {
     
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         
-        //取得locations数组的最后一个
+        // 取得locations数组的最后一个
         let location: CLLocation = locations[locations.count - 1]
         if location.horizontalAccuracy > 0 {
-            let lat = Float(location.coordinate.latitude)
-            let lon = Float(location.coordinate.longitude)
+            let lat = Double(location.coordinate.latitude)
+            let lon = Double(location.coordinate.longitude)
             
             self.currentLocation = location
-            print("[LocationManager didUpdateLocations] lat = \(lat),  lon = \(lon)")
+            print("[OTTLocationManager didUpdateLocations] lat = \(lat),  lon = \(lon)")
             
-            //传值给代理
+            // 传值给代理
             self.delegate?.didUpdateLatLon(self, lat: lat, lon: lon)
             self.manager.stopUpdatingLocation()
+            
+            // 获取反地理编码
+            CLGeocoder().reverseGeocodeLocation(manager.location!, completionHandler: {(placemarks, error) -> Void in
+                
+                if (error != nil) {
+                    print("[OTTLocationManager didUpdateLocations] reverse geocoder failed")
+                    return
+                }
+                
+                if placemarks!.count > 0 {
+                    let pm = placemarks![0] as CLPlacemark
+                    self.delegate?.didUpdateCityDistrict(self, district: pm.subLocality, city: pm.locality, country: pm.country)
+                }
+            })
         }
     }
     
-    //MARK:定位错误信息
+    // 定位错误信息
     func locationManager(manager: CLLocationManager, didFinishDeferredUpdatesWithError error: NSError?) {
-        
-        print("\(error)\(error?.description)")
+        print("[OTTLocationManager didFinishDeferredUpdatesWithError] \(error)")
     }
     
 }
